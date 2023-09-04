ab00134e44e95ccfc832c2c051189b9929c7009c
Anel Hizaguirre <anel93_miel@hotmail.com>
2023-09-04 19:13:35 +0200

    Modern-expl-visua-d3js

--- modern-expl-visua-d3js/main.js
+++ modern-expl-visua-d3js/main.js
@@ -1,158 +1,80 @@
-// CHART START
-// 1. aquí hay que poner el código que genera la gráfica
 const width = 800
 const height = 600
 const margin = {
-    top: 40,
-    bottom: 60,
-    left: 100,
-    right: 20
+    top: 10,
+    right: 10,
+    bottom: 40,
+    left: 70
 }
-
 let years;
 let winners;
 let originalData;
 
-let winnersCount = {};
+const svg = d3.select("#chart")
+    .append("svg")
+    .attr("id", "svg")
+    .attr("width", width)
+    .attr("height", height)
 
-// data:
-d3.csv("WorldCup.csv").then(data => {
-    // 2. aquí hay que poner el código que requiere datos para generar la gráfica
-    data.map(d=> d.Year = +d.Year);
-    
-    originalData = data;
-    years = Array.from(new Set(data.map(d => d.Year))); 
-    winners = filterDataByYear(years[years.length - 1]);
 
-    // Inicializar el recuento de victorias por país
+const elementGroup = svg.append("g")
+    .attr("id", "elementGroup")
+    .attr('transform', `translate(${0}, ${margin.top})`)
+
+let y = d3.scaleBand()
+    .range([0, height - margin.top - margin.bottom])
+    .paddingInner(0.2).paddingOuter(0.05)
+let x = d3.scaleLinear()
+    .range([50, width - margin.left - margin.right])
+
+const axisGroup = svg.append("g")
+    .attr("id", "axisGroup")
+const xAxisGroup = axisGroup
+    .append("g")
+    .attr("id", "xAxisGroup")
+    .attr('transform', `translate(
+        ${margin.left},
+        ${height - margin.bottom})`)
+const yAxisGroup = axisGroup
+    .append("g")
+    .attr("id", "yAxisGroup")
+    .attr('transform', `translate(
+        ${margin.left},
+        ${margin.top})`)
+const xAxis = d3.axisBottom()
+    .scale(x).ticks(5).tickSize(-height)
+const yAxis = d3.axisLeft().scale(y)
+
+d3.csv('WorldCup.csv').then(data => {
+    data.map(d => {
+        d.Year = +d.Year
+    })
+    years = [...new Set(data.map(d => +d.Year))];
+    originalData = data
     winners = d3.nest()
-        .key(d => d.winner)
-        .entries(data)
+        .key(d => d.Winner)
+        .rollup(v => v.length)
+        .entries(data);
 
-    // Actualizar el gráfico con los datos del último año
-    update(winners);
-    slider();
-});
+    // Ordena los datos por cantidad de victorias
+    winners.sort((a, b) => b.value - a.value);
 
-// update:
-function update(data) {
-    // 3. función que actualiza el gráfico
-    // Actualizar el recuento de victorias por país
-    data.forEach(function(d) {
-        if (!winnersCount[d.Winner]) {
-            winnersCount[d.Winner] = 0;
-        }
-        winnersCount[d.Winner] += 1;
-    });
+    // Usa los datos agrupados para el gráfico de barras
+    x.domain([0, d3.max(winners, d => d.value)]);
+    y.domain(winners.map(d => d.key));
 
-    const winnersData = Object.entries(winnersCount).map(([winner, count]) => ({ Winner: winner, Ganadas: count }));
-    
-    winnersData.sort((a, b) => b.Ganadas - a.Ganadas);
+    xAxisGroup.call(xAxis);
+    yAxisGroup.call(yAxis);
+    xAxisGroup.select('.domain').remove();
 
-    // Elimina el gráfico anterior
-    d3.select("svg").remove();
-
-    // Crea un nuevo SVG contenedor para el gráfico de barras
-    var svg = d3.select("#chart")
-        .append("svg")
-        .attr("width", width)
-        .attr("height", height);
-
-    var xScale = d3.scaleLinear()
-        .domain([0, d3.max(winnersData, d => d.Ganadas)]) 
-        .nice()
-        .range([margin.left, width - margin.right]);
-
-    var yScale = d3.scaleBand()
-        .domain(winnersData.map(d => d.Winner)) 
-        .range([margin.top, height - margin.bottom])
-        .padding(0.1);
-
-    svg.selectAll("rect")
-        .data(winnersData)
+    elementGroup.selectAll('.bar')
+        .data(winners)
         .enter()
         .append("rect")
-        .attr("x", margin.left)
-        .attr("y", d => yScale(d.Winner))
-        .attr("width", d => xScale(d.Ganadas))
-        .attr("height", yScale.bandwidth())
+        .attr('class', 'bar')
+        .attr('height', y.bandwidth())
+        .attr('x', margin.left)
+        .attr('y', d => y(d.key))
+        .attr('width', d => x(d.value));
 
-    var xAxis = d3.axisBottom(xScale)
-        .ticks(5)
-        .tickFormat(d3.format("d")); 
-
-    var yAxis = d3.axisLeft(yScale);
-
-    svg.append("g")
-        .attr("class", "x-axis")
-        .attr("transform", `translate(0,${height - margin.bottom})`)
-        .call(xAxis);
-
-    svg.append("g")
-        .attr("class", "y-axis")
-        .attr("transform", `translate(${margin.left},0)`)
-        .call(yAxis);
-
-    svg.append("text")
-        .attr("class", "x-label")
-        .attr("x", width / 2)
-        .attr("y", height - margin.bottom + 50) // Ajusta la posición vertical del texto
-        .text("Veces que ha Ganado");
-
-    svg.append("text")
-        .attr("class", "y-label")
-        .attr("x", -height / 2)
-        .attr("y", margin.left - 80)
-        .attr("transform", "rotate(-90)")
-        .text("Países Ganadores");
-}
-
-// treat data:
-function filterDataByYear(year) {
-    // 4. función que filtra los datos dependiendo del año que le pasemos (year)
-    var selectedYear = parseInt(year);
-
-    // Filtra los datos según el año seleccionado
-    var filteredData = originalData.filter(function(d) {
-        return d.Year === selectedYear;
-    });
-
-    return filteredData;
-    
-}
-// CHART END
-
-// slider:
-function slider() {    
-    // esta función genera un slider:
-    var sliderTime = d3
-        .sliderBottom()
-        .min(d3.min(years))  // rango años
-        .max(d3.max(years))
-        .step(4)  // cada cuánto aumenta el slider (4 años)
-        .width(580)  // ancho de nuestro slider en px
-        .ticks(years.length)  
-        .default(years[years.length -1])  // punto inicio del marcador
-        .on('onchange', val => {
-            // 5. AQUÍ SÓLO HAY QUE CAMBIAR ESTO:
-            //console.log("La función aún no está conectada con la gráfica")
-            const selectedYear = val;
-            const filteredData = filterDataByYear(selectedYear);
-            winnersCount = {};
-            update(filteredData);
-            
-        });
-
-        // contenedor del slider
-        var gTime = d3 
-            .select('div#slider-time')  // div donde lo insertamos
-            .append('svg')
-            .attr('width', width * 0.8)
-            .attr('height', 100)
-            .append('g')
-            .attr('transform', 'translate(30,30)');
-
-        gTime.call(sliderTime);  // invocamos el slider en el contenedor
-
-        d3.select('p#value-time').text(sliderTime.value());  // actualiza el año que se representa
-}
+})
